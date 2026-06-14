fb3b397d694924aa6886442ee08ce2a0797fde9e
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2026-06-14 08:22:36 -0500

    Commit

--- fp.sh
+++ fp.sh
@@ -10,12 +10,13 @@ dev.bragefuglseth.Keypunch \
 io.edcd.EDMarketConnector \
 io.github.plrigaux.sysd-manager \
 io.github.pol_rivero.github-desktop-plus \
-io.missioncenter.MissionCenter \
+io.github.Soundux \
 io.github.wartybix.Constrict \
 md.obsidian.Obsidian \
 me.proton.Mail \
 me.proton.Pass \
 net.runelite.RuneLite \
+org.localsend.localsend_app \
 org.gnome.gitlab.YaLTeR.VideoTrimmer \
 org.signal.Signal \
 ro.go.hmlendea.DL-Desktop \
